a5eeaa950345b1855bb07efe6a1fd62e56d4620a
Daniel Parker <dparker.tech@gmail.com>
2014-06-28 16:21:24 +1000

    DOC: Doxyfile and comments in Semaphore header file

--- ConcurrencyUtilities/Semaphore.h
+++ ConcurrencyUtilities/Semaphore.h
@@ -15,25 +15,61 @@
 #include <mutex>
 #include <condition_variable>
 
+/**
+ *  Semaphore concurrency utility, which can be used to control thread
+ *  access permission.
+ */
 class Semaphore {
 public:
-    /* Constructors */
+    /**
+     *  Default Constructor, which sets the tokens to zero.
+     */
     Semaphore();
+    
+    /**
+     *  Construct a semaphore with nTokens starting tokens.
+     *
+     *  @param  n       The number of tokens to initialise the semaphore with.
+     */
     Semaphore(uint64_t nTokens);
     
-    /* Acquire a token */
+    /**
+     *  Acquire a token. If no tokens are available, the thread will
+     *  block until release is called and the thread is woken up.
+     */
     void Acquire();
     
-    /* Release one token */
+    /**
+     *  Release a single token.
+     */
     void Release();
     
-    /* Release n tokens */
+    /**
+     *  Release n tokens and pulse a single waiting thread.
+     *
+     *  @param  n   The number of tokens to release.
+     */
     void Release(uint64_t nTokens);
     
 protected:
+    /**
+     *  An object to lock for access to class invariants.
+     */
     std::mutex lockObject;
+    
+    /**
+     *  Used to have threads wait, and to pulse threads.
+     */
     std::condition_variable condvar;
+    
+    /**
+     *  The tokens in this Semaphore.
+     */
     uint64_t tokens;
+    
+    /**
+     *  The number of threads that are trying to acquire.
+     */
     uint64_t waitingThreads;
 };
 
